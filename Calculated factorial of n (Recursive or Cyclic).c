53828b57ff1c12fd78ab0b0fdc52cb52cdcf95b4
Haotian Li <2878348186@qq.com>
2022-05-21 19:56:16 +0800

    Create Calculated factorial of n (Recursive or Cyclic).c

---
+++ Calculated factorial of n (Recursive or Cyclic).c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int Fac1(int n)
+{
+	int i = 0;
+	int ret = 1;
+	for (i = 1; i <= n; i++)
+	{
+		ret *= i;
+	}
+	return ret;
+}
+
+//int Fac2(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fac2(n - 1);
+//}
+
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = Fac1(n);                   //ret = Fac2(n);
+	printf("½×³ËÎª:%d\n", ret);
+	return 0;
+}
