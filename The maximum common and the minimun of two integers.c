97f9a58c7fe029d98c7bb1ca127f7916cfe2e497
Haotian Li <2878348186@qq.com>
2022-05-26 07:22:54 +0800

    Create The maximum common and the minimun of two integers.c

---
+++ The maximum common and the minimun of two integers.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a, b, r = 1, i, j;
+	printf("请输入两个整数:");
+	scanf("%d%d", &a, &b);
+	i = a;
+	j = b;
+	while (r != 0)
+		do
+		{
+			r = a % b;
+			a = b;
+			b = r;
+		} while (r != 0);
+		printf("%d和%d的最大公约数是:%d\n", i, j, a);
+		printf("最小公倍数是:%d", i * j / a);
+		return 0;
+}
