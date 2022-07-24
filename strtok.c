6efbf124c63856861300f1c00685a7c56f261159
Li Haotian <2878348186@qq.com>
2022-07-25 00:08:10 +0800

    Create strtok.c

---
+++ strtok.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	char arr[] = "28783@qq.com";
+//	char* p = "@.";
+//	char buf[1024] = { 0 };
+//	strcpy(buf, arr);
+//	for (char* ret = strtok(arr, p); ret != NULL; ret = strtok(NULL, p))
+//	{
+//		printf("%s\n", ret);
+//	}
+//}
